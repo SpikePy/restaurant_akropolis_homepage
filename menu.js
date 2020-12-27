ce2dac21de4ad927ceab26d76c2bd6fea66f1745
Spike Py <hoffelmann@gmail.com>
2020-12-27 21:16:37 +0100

    init

---
+++ menu.js
@@ -0,0 +1,42 @@
+menu = {
+  "Suppen": [
+    {
+      "id": 1,
+      "title": "Griechische Hühnersuppe",
+      "subtitle": "mit Brot",
+      "Price": 3.5
+    },
+    {
+      "id": 2,
+      "title": "Gulaschsuppe",
+      "subtitle": "mit Brot",
+      "Price": 3.5
+    },
+    {
+      "id": 3,
+      "title": "Tomatensuppe",
+      "subtitle": "mit Brot",
+      "Price": 3.5
+    },
+    {
+      "id": 3,
+      "title": "Swiebelsuppe",
+      "subtitle": "mit Brot",
+      "Price": 3.5
+    }
+  ],
+  "Vegetarische_Gerichte": [
+    {
+      "id": 200,
+      "title": "Gemüseauflauf",
+      "subtitle": "verschiedene Gemüsesorten der Saison in Sahnesoße mit Käse überbacken und Salat",
+      "Price": 10.5
+    },
+    {
+      "id": 201,
+      "title": "Gemüseauflauf",
+      "subtitle": "verschiedene Gemüsesorten der Saison in Sahnesoße mit Käse überbacken und Salat",
+      "Price": 10.5
+    }
+  ]
+}
