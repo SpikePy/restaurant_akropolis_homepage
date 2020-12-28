e166c71527ec0068366716f917068c538b7b5a8a
Rene Hoffmann <hoffelmann@gmail.com>
2020-12-28 08:55:48 +0100

    init

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
-Object.keys(menu).forEach(
-  category => {
-    console.log(category)
-    // menu.category.forEach (
-    //   dishes => {
-    //     console.log(dishes)
-    //   }
-    // )
-  }
-)
+function generate_menu() {
+    // get array of categories from menu data and iterate over it to generate html
+    Object.keys(menu).forEach(
+        category => {
+            console.log(`category: ${category}`)
+
+            var entry = document.createElement("tr")
+            entry.innerHTML = `<td colspan="3"><h3>${category}</h3></td>`
+            html_menu.appendChild(entry)
+
+            // iterate over dishes of coresponding category and generate html
+            menu[category].forEach(
+                dish => {
+                    console.log(`${dish.id} ${dish.title} ${dish.subtitle} ${dish.special_ingedients} ${dish.price}`)
+
+                    var entry = document.createElement("tr")
+                    entry.innerHTML = `
+                        <td>${dish.id}</td>
+                        <td class="dish-description"><span class="dish-title">${dish.title}</span> ${dish.subtitle}<sup>${dish.special_ingedients}</sup></td>
+                        <td>${dish.price.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' })}</td>`
+                    html_menu.appendChild(entry)
+                }
+            )
+        }
+    )
+}
